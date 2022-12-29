0fff720dee0a6ae8e4de8c8c35e5d0a94d47f3c7
Nithin-47 <100336015+Nithin-47@users.noreply.github.com>
2022-12-29 22:01:14 +0530

    Add files via upload

--- linked_list.C
+++ linked_list.C
@@ -1,10 +1,10 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 
 struct node{
 int value;
 struct node *next;
-}
+};
 
 
 typedef struct node *NODE;
@@ -59,7 +59,7 @@ NODE insert_end(NODE first,int item)
     if(first==NULL)
         return New;
    
-    if(first->next=NULL)
+    if(first->next==NULL)
     {
     first->next=New;
     return first;
@@ -85,7 +85,7 @@ NODE insert_end(NODE first,int item)
 NODE delete_beg(NODE first)
 {
     NODE temp;
-    temp=getnode();
+    // temp=getnode();
 
     if(first==NULL)
     {
@@ -96,7 +96,7 @@ NODE delete_beg(NODE first)
     temp=first;
     temp=temp->next;
 
-    printf("Item has been deleted:%d",first->value);
+    printf("Item has been deleted:%d\n",first->value);
     free(first);
 
     return(temp);
@@ -115,8 +115,8 @@ NODE delete_end(NODE first)
         return NULL;
     }
 
-    prev=getnode();
-    curr=getnode();
+    // prev=getnode();
+    // curr=getnode();
 
     prev=NULL;
     curr=first;
@@ -132,20 +132,156 @@ NODE delete_end(NODE first)
     return(first);
 }
 
+
+
+NODE insert_at_any_position(int item, int position, NODE first)
+{
+    NODE curr,newnode,prev;
+    int count=1;
+
+    newnode=getnode();
+    newnode->value=item;
+    newnode->next=NULL;
+
+
+    if((first==NULL)&&(pos==1))
+        return newnode;
+
+    prev=NULL;
+    curr=first;
+
+    while((count!=pos)&&(curr!=NULL))
+    {
+        prev=curr;
+        curr=curr->next;
+        count++;
+
+    }
+
+    if(count==pos)
+    {
+        prev->next=newnode;
+        newnode->next=curr;
+        return first;
+    }
+
+    if(curr==NULL)
+    {
+        printf("Position not found");
+        return first;
+    }
+
+
+
+}
+
+
+NODE del_specific(NODE first,int key)
+{
+    NODE prev,curr;
+
+    if(first==NULL)
+    {    printf("Nothing to delete");
+         return NULL;
+    }
+
+    curr=first;
+    prev=NULL;
+
+    if(curr->value==key)
+    {
+        printf("Item deleted:%d",curr->value);
+        first=first->next;
+        free(curr);
+        return(first);
+    }
+
+    while((curr->value!=key)&&(curr!=NULL))
+    {
+        prev=curr;
+        curr=curr->next;
+    }
+
+    if(curr->value==key)
+    {
+        prev->next=curr->next;
+        printf("Item deleted:%d",curr->value);
+        free(curr);
+        return first;
+    }
+
+    if(curr==NULL)
+    {
+        printf("End of list reached but item wasnt found.")
+        return first;
+    }
+}
+
+
 //For displaying-
 
 void display(NODE first)
 {
     NODE temp;
-    temp=getnode();
+    // temp=getnode();
     temp=first;
 
-    while(temp!->NULL)
+    while(temp!=NULL)
     {
-        printf("\nValue stored in node:%d",temp->value);
+        printf(" %d",temp->value);
         temp=temp->next;
     }
 }
 
 
-
+int main()
+{
+    int choice, item, x;
+    NODE first = NULL;
+    while (1)
+    {
+        printf("\n\nMenu\n-----------------------------------------\n1) Insert at beginning\n2) Insert at end\n3) Display\n4) Delete at Beginning\n5) Delete at the end\n6) Insert at any Position\n 7) Delete a specific Value\n8) Exit\n-----------------------------------------\nEnter your choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the element to be inserted : ");
+            scanf("%d", &x);
+            first = insert_beg(first,x);
+            break;
+        case 2:
+            printf("Enter the element to be inserted : ");
+            scanf("%d", &x);
+            first = insert_end(first,x);
+            break;
+        case 3:printf("/nThe items are:");
+            display(first);
+            break;
+        case 4:
+            first = delete_beg(first);
+            printf("Item has been removed from the first NODE");
+            break;
+        case 5:
+            first = delete_end(first);
+            printf("Item has been removed from the last node.");
+            break;
+        case 6:
+            printf("Enter the element to be inserted : ");
+            scanf("%d", &pos);
+            printf("Enter the element to be inserted : ");
+            scanf("%d", &x);
+            insert_at_any_position(x,pos,first);
+            break;
+        case 7:
+            first=del_specific()
+            printf("Item has been removed from the last node.");
+            break;
+        case 8:
+            exit(0);
+            break;
+        default:
+            printf("Please enter the correct value.");
+        }
+    }
+    return 0;
+}
